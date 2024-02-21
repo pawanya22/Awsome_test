356493696e0244fa150b2db4027d681551c22f29
Pawanya <kariyawasampawanya@gmail.com>
2024-02-21 08:15:54 +0530

    create project

--- Awesome/App.tsx
+++ Awesome/App.tsx
@@ -1,41 +1,13 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
 
-
-export default class App extends React.Component {
-  render() {
-    return(
-    <View style={styles.container}>
-        <Text>open up App to start working on your app!</Text>
-        <Text>New git hub</Text>
-    </View>
-  );
-  }
-  
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#32a89e',
-    alignItems:'center',
-    justifyContent:"center"
-  }
-})
-
-class Lockscreen extends Component {
-  constructor (props:any) {
-    super(props);
-    this.state = {};
-  }
-
+export default class App extends Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <StatusBar barStyle={"light-content"}/>
-        <Text> LocksScreenPasscode </Text>
-      </SafeAreaView>
-    );
+      <View>
+        <Text>App</Text>
+      </View>
+    )
   }
-
 }
+
